bad2d34d63e70927926e7bf74a9793d4ffb7e775
CheongJF <116238902+CheongJF@users.noreply.github.com>
2023-10-26 15:42:46 +0800

    Delete check_grammar.sh

--- check_grammar.sh
+++
@@ -1,9 +0,0 @@
-!/bin/bash
-# Install LanguageToolsudo apt-get install languagetool
-# Run LanguageTool and output results to a file
-languagetool -l en-US /path/to/your/code > grammar_results.txt
-# Check if there are any grammar errorsif [ -s grammar_results.txt ]; then
-  echo "Grammar errors found:"  cat grammar_results.txt
-  exit 1else
-  echo "No grammar errors found."  exit 0
-fi
